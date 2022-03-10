8918ff811f3c6cdf3896d50c216cf7cc4e407db2
Aritesoma <aritesomaejuliuwa@gmail.com>
2022-03-10 13:31:25 -0800

    Use the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print all that it contains
+(*
+ * Description: Printing a sentence)?
+ * Return: The returning value is 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return 0;
+}
