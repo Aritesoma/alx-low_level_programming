0f1625049c5a49646444bbf36a16db3f873cf63c
Aritesoma <aritesomaejuliuwa@gmail.com>
2022-03-11 14:46:35 -0800

    0-9 num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all in it
+ *
+ * Description: printing list of no in base 10
+ * Return: Value is 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%i", n);
+}
+putchar('\n');
+
+
+return (0);
+}
