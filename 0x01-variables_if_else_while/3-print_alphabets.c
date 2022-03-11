a3a7cddb701e40bdb8aace6da53817211ef7489b
Aritesoma <aritesomaejuliuwa@gmail.com>
2022-03-11 12:14:01 -0800

    printing both cap and low alphabetes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print all in it
+ *
+ * Description: prints both lower and upper alp)?
+ * Return: Returned value is 0
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch ='A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+
+}
