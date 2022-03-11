798af83c94238942cdfcd3e1978e015aa218670e
Aritesoma <aritesomaejuliuwa@gmail.com>
2022-03-11 11:48:31 -0800

    a test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints everything it contains
+ *
+ * Description: Prints all aphabets in lower case)?
+ * Return: No return function
+ */
+
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+
+return (0);
+
+
+
+
+
+
+
+}
