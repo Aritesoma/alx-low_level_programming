8859434136c12e2dca930eb27fcdd5ed366c2bfd
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-15 20:29:41 +0000

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabets printing
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+}
