25ce4929d9ab1371f1960d22ea02d477328b7b33
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-15 22:36:38 +0000

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islower(int c) - checks for lowercase letters
+ * _islower - checks for lowercase letters
  * @c: the character it checks
  * Return: 1 if c is lowercase, 0 if not
  */
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,14 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabet
- * @c 
+ * @c: The char it checks
+ * Return: 1 if c is an alphabet
+ */
+int _isalpha(int c)
+{
+	return (c != 0);
+
+
+
+
+}
