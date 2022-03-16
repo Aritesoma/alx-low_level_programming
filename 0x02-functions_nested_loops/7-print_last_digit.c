13665132e848a12732f13bfef705005a43b411c2
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-16 12:32:49 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	if (i < 0)
+	{
+		i *= -i;
+	}
+	_putchar(i + '0');
+	return (i);
+}
