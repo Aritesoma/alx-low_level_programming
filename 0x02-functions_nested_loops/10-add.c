0344141cc48e5806825a57651e863fa2176e7ec5
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-16 18:51:26 +0000

    my adddition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ * @a: first parameter
+ * @b: 2nd parameter
+ *
+ * Return: valued 0
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int _isalpha(int c);
 int _abs(int);
 int print_sign(int n);
 int print_last_digit(int);
+int add(int, int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
