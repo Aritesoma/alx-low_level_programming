50a420eb984b0d074623839fcca99a3ef01f049f
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-17 21:36:48 +0000

    checks digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _isdigit - Function to  check digit
+ * @c: Value
+ *
+ * Description: Printing checked digit
+ * Return: Return 1 if c is a digit else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+}
