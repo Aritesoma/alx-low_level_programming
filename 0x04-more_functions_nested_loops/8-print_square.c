bb0556259840bd09e9b8072fe053b780ae044332
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-18 12:05:37 +0000

    square printing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - printing a nice squ
+ * @size: the size of square
+ *
+ * Return: valued 0
+ */
+
+void print_square(int size)
+{
+	int squ = 0;
+
+	while (squ < size)
+	{
+		_putchar('#');
+		squ++;
+
+	}
+
+	_putchar('\n');
+
+
+}
