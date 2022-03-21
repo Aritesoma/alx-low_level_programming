adf2cb43923c0d0e6978f1ccf76ef30d6807c9af
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-21 20:55:35 +0000

    char in a stri n g

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+es (18 sloc)  248 Bytes
+
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ *@str: the string to use.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
