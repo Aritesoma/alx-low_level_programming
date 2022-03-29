04f4997343e7fe02f5f76100a3bdaecc88d767d2
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-29 03:52:51 +0000

    diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Printing two diagonals of a matrics
+ * @a: Pointer
+ * @size: Size od diagonal
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+	int i, j, sum1 = 0, sum2 = 0;
+
+	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
+		sum1 += a[j], sum2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
+
+
+
+
+
+
+
+}
