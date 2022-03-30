b0e104eec8f6028ece0eef6640e81ff4ef1fb7cc
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-03-30 20:27:42 +0000

    to the power of

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns to the power
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	if (y == 1)
+	{
+		return (x);
+	}
+
+	return (_pow_recursion(x, y));
+
+}
