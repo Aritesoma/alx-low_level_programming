9ef20fe2bef54b878f3d9d8e3077b87501daf336
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-04-12 03:20:54 +0000

    the dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+/**
+ * struct dog - details of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: Prints details of a dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
